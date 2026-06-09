dd590ccdb15f8dc52bf3faea712de30d1d97aeab
fanghey <2603Lysenko2603@gmail.com>
2026-06-09 13:22:01 +0200

    Update test_restore_names.py

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,5 +1,4 @@
-import pytest
-from app.restore_names import restore_names
+from app.main import restore_names
 
 
 def test_restore_names_when_first_name_is_none() -> None:
@@ -48,7 +47,19 @@ def test_restore_names_with_multiple_mixed_users() -> None:
     ]
     restore_names(users)
     assert users == [
-        {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
-        {"first_name": "John", "last_name": "Doe", "full_name": "John Doe"},
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        },
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "John Doe"
+        },
     ]
