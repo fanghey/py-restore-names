d81e6c9a778ccd907318914a31f4a9c0cc53cad3
fanghey <2603Lysenko2603@gmail.com>
2026-06-09 13:20:02 +0200

    Update test_restore_names.py

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -2,15 +2,19 @@ import pytest
 from app.restore_names import restore_names
 
 
-def test_restore_names_when_first_name_is_none():
-    users = [{"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"}]
+def test_restore_names_when_first_name_is_none() -> None:
+    users = [{
+        "first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy"
+    }]
     restore_names(users)
     assert users == [
         {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}
     ]
 
 
-def test_restore_names_when_first_name_is_missing():
+def test_restore_names_when_first_name_is_missing() -> None:
     users = [{"last_name": "Adams", "full_name": "Mike Adams"}]
     restore_names(users)
     assert users == [
@@ -18,21 +22,25 @@ def test_restore_names_when_first_name_is_missing():
     ]
 
 
-def test_restore_names_does_not_overwrite_existing_first_name():
-    users = [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}]
+def test_restore_names_does_not_overwrite_existing_first_name() -> None:
+    users = [{
+        "first_name": "Jack",
+        "last_name": "Holy",
+        "full_name": "Jack Holy"
+    }]
     restore_names(users)
     assert users == [
         {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}
     ]
 
 
-def test_restore_names_with_empty_list():
+def test_restore_names_with_empty_list() -> None:
     users = []
     restore_names(users)
     assert users == []
 
 
-def test_restore_names_with_multiple_mixed_users():
+def test_restore_names_with_multiple_mixed_users() -> None:
     users = [
         {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
         {"last_name": "Adams", "full_name": "Mike Adams"},
